0f5c54c7be10ea73ac56bcc2f732b53c46b42b10
faizal <faizal@Faizals-MBP>
2025-06-29 14:39:10 +0400

    graph is created and system prompt created

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -5,14 +5,33 @@ from langgraph.prebuilt import ToolNode, tools_condition
 
 
 class GraphBuilder():
-    def __init__(self, graph):
-        pass
+    def __init__(self):
+        self.tools = [
 
-    def agent_function(self):
-        pass
+        ]
+
+        self.system_prompt = SYSTEM_PROMPT
+
+    def agent_function(self, state: MessagesState):
+        """ Main Agent Function """
+        user_question  = state["messages"]
+        user_input = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(user_input)
+        return {"messages":[response]}
+
 
     def build_graph(self):
-        pass
+        graph_builder = StateGraph(MessagesState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START, "agent")
+        graph_builder.add_conditional_edge("agent",tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent", END)
+
+        self.graph = graph_builder.compile()
+        return self.graph
 
     def __call__(self, *args, **kwds):
-        pass
+        return self.build_graph()
+    
