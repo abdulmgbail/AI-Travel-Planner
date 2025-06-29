b6da2c58b7dc597242308c5b85592c025d9cac74
faizal <faizal@Faizals-MBP>
2025-06-29 12:19:57 +0400

    confog and load model functions

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,18 @@
+from utils.model_loaders import ModelLoader
+from prompt_library.prompt import SystetmPrompt
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+class GraphBuilder():
+    def __init__(self, graph):
+        pass
+
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+    def __call__(self, *args, **kwds):
+        pass
